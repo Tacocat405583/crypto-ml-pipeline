6b10ffa6ba94d8e409100d8a901d81f1cbfec72f
unknown <nicolashernan2029@gmail.com>
2026-07-09 21:35:04 -0700

    Simple Coinbase price check

--- src/tracker/coinbase.py
+++ src/tracker/coinbase.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+import requests
 
 
+url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
 
+response = requests.get(url)
+
+print(response.status_code)
+print(response.text)
+
+data = response.json()
+print(data)
 
 
 
