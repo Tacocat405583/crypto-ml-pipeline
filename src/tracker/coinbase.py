94503718f005d105b466f1865425babb1233aac9
unknown <nicolashernan2029@gmail.com>
2026-07-17 23:24:33 -0700

    Commented out simple response code, Now working on adding data to json

--- src/tracker/coinbase.py
+++ src/tracker/coinbase.py
@@ -3,13 +3,39 @@ import requests
 
 url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
 
-response = requests.get(url)
+# response = requests.get(url)
 
-print(response.status_code)
-print(response.text)
+# print(response.status_code)
+# print(response.text)
 
-data = response.json()
-print(data)
+# data = response.json()
+
+# print("This is after parsing")
+# print(data)
+
+class CoinbaseClient:
+
+    def __init__(self,url):
+        self._url = url
+
+
+    def get_spot_price(self):
+
+        response = requests.get(self._url)
+        data = response.json()
+
+        return data
+    
+    def append_json(self):
+
+        
+    
+
+if __name__ == "__main__":
+    client = CoinbaseClient(url)
+    print(client.get_spot_price())
+
+
 
 
 
