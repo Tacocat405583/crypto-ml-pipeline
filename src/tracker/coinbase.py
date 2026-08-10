3f8cfb4ee1e7c37c9b73017b3da119b2ae68d5a9
unknown <nicolashernan2029@gmail.com>
2026-08-10 02:30:00 -0700

    Consolidated excepts and attempting calls multiple times

--- src/tracker/coinbase.py
+++ src/tracker/coinbase.py
@@ -3,9 +3,10 @@ import requests
 from datetime import datetime, timezone
 from pathlib import Path
 import sys
+import time
 
 url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
-
+ATTEMPTS = 3
 
 file_path = Path(__file__).parent / "ticker_price.jsonl"
 
@@ -35,22 +36,24 @@ class CoinbaseClient:
 
     def _fetch(self):
 
-        try:
-            response = requests.get(self._url,timeout=(5,10))
+        for i in range(ATTEMPTS):
 
-            if response.status_code == 200:
-                data = response.json()
-                return data
-            else:
-                return False
-        except requests.exceptions.ConnectTimeout:
-            print("The connection timed out.")
-        except requests.exceptions.ReadTimeout:
-            print("The server took too long to send data.")
-        except requests.exceptions.Timeout:
-            print("The request timed out generally.")
-        except requests.RequestException as e:
-            print(f"Error: {e}",file=sys.stderr)
+            try:
+                #give time to get response and pass data
+                response = requests.get(self._url,timeout=(5,10))
+
+                if response.status_code == 200:
+                    data = response.json()
+                    return data
+            except requests.RequestException as e:
+                print(f"Error: {e}",file=sys.stderr)
+                if i < ATTEMPTS - 1: # one less sleep
+                    time.sleep(10)
+                continue
+
+
+        return False
+                
 
 
 
