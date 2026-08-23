ae35d33bb7d5cd26b6f799efc5e9b95a6580da6a
unknown <nicolashernan2029@gmail.com>
2026-08-22 20:47:36 -0700

    Add C++ feed handler skeleton with CMake build

---
+++ services/feed-handler/src/main.cpp
@@ -0,0 +1,11 @@
+// Reference:
+//   Beast sync-ssl websocket client (start here):
+//     https://github.com/boostorg/beast/tree/develop/example/websocket/client/sync-ssl
+//   Beast docs:
+//     https://www.boost.org/doc/libs/release/libs/beast/doc/html/
+//   Coinbase Exchange websocket:
+//     https://docs.cdp.coinbase.com/exchange/docs/websocket-overview
+
+int main() {
+    return 0;
+}
