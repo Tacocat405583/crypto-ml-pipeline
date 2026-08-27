79205fa35abd2870614a48d0c84a06edcc407267
unknown <nicolashernan2029@gmail.com>
2026-08-27 02:07:13 -0700

    Thread safe wait and pop

--- services/feed-handler/src/queue.cpp
+++ services/feed-handler/src/queue.cpp
@@ -18,8 +18,14 @@ public:
         data_queue.push(std::move(new_value));
         data_cond.notify_one();
     }
+    void wait_and_pop(T& value)
+    {
+        std::unique_lock<std::mutex> lk(mut);
+        data_cond.wait(lk, [this]{return !data_queue.empty();});
+        value=std::move(data_queue.front());
+        data_queue.pop();
+    }
     
-
 
 
 
